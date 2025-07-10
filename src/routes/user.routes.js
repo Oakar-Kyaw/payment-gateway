bd7304bf37c771c1a4f411fb89fafe7553e7e8b7
Oakar-Kyaw <oakarkyaw7090@gmail.com>
2025-07-10 14:55:15 +0630

    adding user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,7 +4,11 @@ const router = require('express').Router()
 
 router.get('/users', (req, res) => {
     res.json({
+      success: true,
       message: "User route",
+      data: [{
+        name: "Oakar Kyaw",
+      }]
     });
 }); 
 
